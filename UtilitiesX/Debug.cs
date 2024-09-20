dbb47ef4ca7f588a69301a134c481ac671907eaa
MohammadAyaad <155004022+MohammadAyaad@users.noreply.github.com>
2024-09-20 13:54:32 +0300

    feat: added DebugExtensions

---
+++ UtilitiesX/Debug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesX
+{
+    public static class DebugExtensions
+    {
+        public static void Println(this string str)
+        {
+            Console.WriteLine(str);
+        }
+        public static void Println<T>(this T o,Func<T,string> str)
+        {
+            Console.WriteLine(str(o));
+        }
+        public static T PrintlnRt<T>(this T o, Func<T, string> str)
+        {
+            Console.WriteLine(str(o));
+            return o;
+        }
+
+    }
+}
