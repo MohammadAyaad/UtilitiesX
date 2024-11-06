c72d621c00ed8b9b24a25b8af846e6bc4c2f650e
MohammadAyaad <155004022+MohammadAyaad@users.noreply.github.com>
2024-11-06 08:12:41 +0200

    feat: added GenerateArray

---
+++ UtilitiesX/ArrayUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesX
+{
+    public static class ArrayUtilities
+    {
+        public static T[] GenerateArray<T>(Func<int, T> generator, int count)
+        {
+            T[] arr = new T[count];
+            for (int i = 0; i < count; i++)
+                arr[i] = generator(i);
+            return arr;
+        }
+    }
+}
