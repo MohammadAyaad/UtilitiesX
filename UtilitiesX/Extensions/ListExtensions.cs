99c9eecc4836dda330abf6cabafdebd65a499cc9
MohammadAyaad <155004022+MohammadAyaad@users.noreply.github.com>
2024-11-06 08:09:35 +0200

    fix: fixed spelling of "Extentions" => "Extensions"

--- UtilitiesX/Extensions/ListExtensions.cs
+++ UtilitiesX/Extensions/ListExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UtilitiesX.Extentions
+namespace UtilitiesX.Extensions
 {
     public static class ObjectExtensions
     {
