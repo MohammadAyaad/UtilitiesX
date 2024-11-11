895a2093606f6eb81d8eced2e8806415a47ffcc8
MohammadAyaad <155004022+MohammadAyaad@users.noreply.github.com>
2024-11-11 12:16:11 +0200

    feat: added Flatten and FlattenTransform Functions

---
+++ UtilitiesX/Extensions/ObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesX.Extensions;
+
+public static class ObjectExtensions
+{
+    public static V Transform<T, V>(this T obj, Func<T, V> transformer)
+    {
+        return transformer(obj);
+    }
+    public static ICollection<T> Flatten<T>(this T obj, Func<T,ICollection<T>> childrenOf)
+    {
+        return new List<T>()
+            .Expand(childrenOf(obj).ForEachCollect(o => o.Flatten(childrenOf)).Collect())
+            .Expand(obj);
+    }
+    public static ICollection<V> FlattenTransform<T,V>(this T obj, Func<T, ICollection<T>> childrenOf, Func<T, V> transformer)
+    {
+        return new List<V>()
+            .Expand(childrenOf(obj).ForEachCollect(o => o.FlattenTransform(childrenOf, transformer)).Collect())
+            .Expand(transformer(obj));
+    }
+}
